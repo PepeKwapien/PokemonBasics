a4346262343f9272233f5777dbdbf799fcd51216
PepeKwapien <pppkwapien@gmail.com>
2023-01-25 22:28:54 +0100

    Configured service collection

--- ExternalApiHandler/Program.cs
+++ ExternalApiHandler/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccess;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace ExternalApiHandler
 {
@@ -7,12 +10,21 @@ namespace ExternalApiHandler
         static void Main(string[] args)
         {
             IConfiguration config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
+               .AddJsonFile("appsettings.json")
+               .Build();
 
-            ExternalApiSettings settings = config.GetRequiredSection("ExternalApiSettings").Get<ExternalApiSettings>();
+            var serviceProvider = new ServiceCollection()
+                .Configure<ExternalApiSettings>(config.GetRequiredSection("ExternalApiSettings"))
+                .AddDbContext<PokemonDatabaseContext>()
+                .AddHttpClient()
+                .BuildServiceProvider();
 
-            Console.WriteLine(settings.BaseUrl);
+            using(var scope = serviceProvider.CreateScope())
+            {
+                var settings = scope.ServiceProvider.GetService<IOptions<ExternalApiSettings>>().Value;
+
+                Console.WriteLine(settings.BaseUrl);
+            }
         }
     }
 }
