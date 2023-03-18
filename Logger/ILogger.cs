5acd1f3ede1f80f2f675440fa08b497a557af4f4
PepeKwapien <pppkwapien@gmail.com>
2023-03-18 16:33:02 +0100

    Added ILogger

---
+++ Logger/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Logger
+{
+    public interface ILogger
+    {
+        // Methods to log messages
+        bool Debug(string message);
+        bool Info(string message);
+        bool Warn(string message);
+        bool Success(string message);
+        bool Error(string message);
+
+        // Methods to fully set path
+        string SetLogPath(string path);
+        string SetFileName(string fileName);
+
+        // Methods for fluent API to manage path
+        ILogger AddToPath(string directoryName);
+        ILogger GoBackToParentDirectory();
+    }
+}
