461d2cf72f6c1206b622e9b0521fec275ba6ea5c
PepeKwapien <pppkwapien@gmail.com>
2023-04-02 16:51:29 +0200

    Fixed pokedex dto

--- ExternalApiHandler/DTOs/PokedexDto.cs
+++ ExternalApiHandler/DTOs/PokedexDto.cs
@@ -6,7 +6,7 @@
         public string name { get; set; }
         public NameWithLanguage[] names { get; set; }
         public PokemonEntry[] pokemon_entries { get; set; }
-        public string? region { get; set; }
+        public Name? region { get; set; }
         public Name[] version_groups { get; set; }
     }
 }
