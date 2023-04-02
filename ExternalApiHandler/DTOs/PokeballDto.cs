59b175f31d56af67d67ebb74433ebabcab9c432b
PepeKwapien <pppkwapien@gmail.com>
2023-04-02 16:48:27 +0200

    Fixed pokeball dto

--- ExternalApiHandler/DTOs/PokeballDto.cs
+++ ExternalApiHandler/DTOs/PokeballDto.cs
@@ -3,7 +3,7 @@
     internal class PokeballDto : IDto
     {
         public string name { get; set; }
-        public NameWithLanguage names { get; set; }
+        public NameWithLanguage[] names { get; set; }
         public EffectEntry[] effect_entries { get; set; }
         public GameIndice[] game_indices { get; set; }
     }
