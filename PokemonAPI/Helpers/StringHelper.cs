496ca446247638783a11009927fa15bbfa142820
PepeKwapien <pppkwapien@gmail.com>
2023-07-21 19:56:04 +0200

    Levenshtein distance function

---
+++ PokemonAPI/Helpers/StringHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace PokemonAPI.Helpers
+{
+    public class StringHelper
+    {
+        public static int LevenshteinDistance(string firstLiteral, string secondLiteral)
+        {
+            int firstLiteralLength = firstLiteral.Length;
+            int secondLiteralLength = secondLiteral.Length;
+            int[,] dimension = new int[firstLiteralLength + 1, secondLiteralLength + 1];
+
+            for(int i = 0; i <= firstLiteralLength; i++)
+            {
+                dimension[i, 0] = i;
+            }
+            for(int i = 1; i <= secondLiteralLength; i++)
+            {
+                dimension[0, i] = i;
+            }
+
+            for (int i = 1; i <= firstLiteralLength; i++)
+            {
+                for (int j = 1; j <= secondLiteralLength; j++)
+                {
+                    int cost = firstLiteral[i - 1] != secondLiteral[j - 1] ? 1 : 0;
+                    dimension[i, j] = new[] { dimension[i - 1, j] + 1, dimension[i, j - 1] + 1, dimension[i - 1, j - 1] + cost }.Min();
+                }
+            }
+
+            return dimension[firstLiteralLength, secondLiteralLength];
+        }
+    }
+}
---
+++ Tests/PokemonAPI/Helpers/StringHelperTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokemonAPI.Helpers;
+
+namespace Tests.PokemonAPI.Helpers
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void Levenshtein_Zero()
+        {
+            // Arrange
+            string string1 = "bulbasaur";
+            string string2 = "bulbasaur";
+
+            // Act
+            int result = StringHelper.LevenshteinDistance(string1, string2);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Levenshtein_One()
+        {
+            // Arrange
+            string string1 = "monkey";
+            string string2 = "mankey";
+
+            // Act
+            int result = StringHelper.LevenshteinDistance(string1, string2);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Levenshtein_Eight()
+            // Eight (squirtle's length) char replacements
+        {
+            // Arrange
+            string string1 = "squirtle";
+            string string2 = "pikachu";
+
+            // Act
+            int result = StringHelper.LevenshteinDistance(string1, string2);
+
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+    }
+}
